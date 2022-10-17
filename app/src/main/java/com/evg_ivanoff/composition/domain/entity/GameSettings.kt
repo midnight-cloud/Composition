28788b334bcfc661c22cc823f9231c322a092f01
Eugene Ivanov <wied11@yandex.ru>
2022-10-17 22:03:40 +0500

    serializable заменены на parcelable

--- app/src/main/java/com/evg_ivanoff/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/evg_ivanoff/composition/domain/entity/GameSettings.kt
@@ -1,10 +1,12 @@
 package com.evg_ivanoff.composition.domain.entity
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val minPercentsOfRightAnswers: Int,
     val gameTimeInSeconds: Int
-): Serializable
+): Parcelable

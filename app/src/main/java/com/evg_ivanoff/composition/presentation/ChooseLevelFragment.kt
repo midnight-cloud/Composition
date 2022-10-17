436afdf5ab1f9132c7d5decb908931d42000187b
Eugene Ivanov <wied11@yandex.ru>
2022-10-17 18:36:18 +0500

    реализация viewbinding на фрагментах

--- app/src/main/java/com/evg_ivanoff/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/evg_ivanoff/composition/presentation/ChooseLevelFragment.kt
@@ -6,14 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.evg_ivanoff.composition.R
+import com.evg_ivanoff.composition.databinding.FragmentChooseLevelBinding
 
-class ChooseLevelFragment: Fragment() {
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/evg_ivanoff/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/evg_ivanoff/composition/presentation/GameFinishedFragment.kt
@@ -6,14 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.evg_ivanoff.composition.R
+import com.evg_ivanoff.composition.databinding.FragmentGameFinishedBinding
 
-class GameFinishedFragment: Fragment() {
+class GameFinishedFragment : Fragment() {
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    ): View {
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
